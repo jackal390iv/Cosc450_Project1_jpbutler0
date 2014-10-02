0fc7615effba5d0315855229481dd15b739aad1b
alfred <jonpbiv@gmail.com>
2014-10-02 15:04:30 -0400

    created C class

---
+++ Cosc450_Project1_jpbutler0/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Jonathan Butler
+ * Class: COSC 450- Project 1
+ *
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
