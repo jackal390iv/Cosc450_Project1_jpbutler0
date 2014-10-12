bdbb45dd24ac28f2dce708a291c6a3ee01d49bda
alfred <jonpbiv@gmail.com>
2014-10-12 06:36:23 -0400

    updated

--- Cosc450_Project1_jpbutler0/main.cpp
+++ Cosc450_Project1_jpbutler0/main.cpp
@@ -19,39 +19,75 @@ using namespace std;
  */
 int main(int argc, char** argv) {
 
-    int val = 1;
     FILE *file = NULL;
 
-    do {
+    char location[256];
+    printf("Please input the location of the data.txt file: ");
+    //fgets(location, 256, stdin);
+    strcpy(location, "/home/alfred/Desktop/data.txt");
+    printf("You have entered: %s \n", location);
+    file = fopen(location, "r");
 
-        char location[256];
-        printf("Please input the location of the data.txt file: ");
-        fgets(location, 256, stdin);
-        strcpy(location, "/home/alfred/Desktop/data.txt");
-        printf("You have entered: %s \n", location);
-        file = fopen(location, "r");
+    if (file == NULL) {
+        printf("ERROR: failed to find data.txt file, please try again. \n");
+        exit(0);
+    } else {
+        printf("PASS: data.txt file has been found. \n");
 
-        if (file == NULL) {
-            printf("ERROR: failed to find data.txt file, please try again \n");
-        } else {
-            printf("PASS: data.txt file has been found. \n");
+        struct stat st;
+        stat(location, &st);
+        int size = st.st_size;
+        char bef[size];
 
-            struct stat st;
-            stat(location, &st);
-            int size = st.st_size;
-            char bef[size];
-            
-            int k=0;
-            fscanf(file, "%d", &k);
-            while(!feof(file)){
-                printf("%d\n",k);
-                fscanf(file, "%d", &k);
-            }
-            
+        int array[size];
+        ;
 
+        int k = 0;
+        while (!feof(file)) {
+            fscanf(file, "%d", &array[k]);
+            k++;
         }
-        break;
-    } while (val = 1);
+
+        int length = 0;
+        int count = 0;
+        while ((count < 2)) {
+            if (array[length] == 0) {
+                count++;
+            } else {
+                count = 0;
+            }
+            length++;
+        }
+
+        size = length - 2;
+
+        int fixed[size];
+        int l = 0;
+        while (l < size) {
+            int tmp = array[l];
+            fixed[l] = tmp;
+            l++;
+        }
+
+        /* int i = 0;
+         while (i < size) {
+             printf("%d\n", fixed[i]);
+             i++;
+         }*/
+
+        if (!(size % 10 == 0)) {
+            printf("ERROR: number of integers are not a multiple of 10, please try again.\n");
+            exit(0);
+        } else {
+            printf("PASS: number of integers is a multiple of 10.\n");
+        }
+        fclose(file);
+        
+        
+        
+        
+        
+    }
 
     return 0;
 }
