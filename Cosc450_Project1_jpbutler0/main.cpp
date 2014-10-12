54a4089f7c03bc5ba1b41e3baeeb3dbce83b4cca
alfred <jonpbiv@gmail.com>
2014-10-12 05:16:14 -0400

    updated

--- Cosc450_Project1_jpbutler0/main.cpp
+++ Cosc450_Project1_jpbutler0/main.cpp
@@ -8,6 +8,9 @@
 #include <cstdlib>
 #include<stdio.h>
 #include<string.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h>
 
 using namespace std;
 
@@ -16,30 +19,40 @@ using namespace std;
  */
 int main(int argc, char** argv) {
 
-    int val=1;
-    FILE *file=NULL;
-    
-    do{
-        
+    int val = 1;
+    FILE *file = NULL;
+
+    do {
+
         char location[256];
         printf("Please input the location of the data.txt file: ");
         fgets(location, 256, stdin);
-        strcpy(location,"/home/alfred/Desktop/data.txt");        
-        printf("You have entered: %s \n",location);        
-        file=fopen(location,"r");
-               
-        if(file==NULL){
-            printf("ERROR: failed to find data.txt file, please try again");
+        strcpy(location, "/home/alfred/Desktop/data.txt");
+        printf("You have entered: %s \n", location);
+        file = fopen(location, "r");
+
+        if (file == NULL) {
+            printf("ERROR: failed to find data.txt file, please try again \n");
+        } else {
+            printf("PASS: data.txt file has been found. \n");
+
+            struct stat st;
+            stat(location, &st);
+            int size = st.st_size;
+            char bef[size];
+            
+            int k=0;
+            fscanf(file, "%d", &k);
+            while(!feof(file)){
+                printf("%d\n",k);
+                fscanf(file, "%d", &k);
+            }
+            
+
         }
-        else{
-            printf("PASS: data.txt file has been found.");
-            
-            
-            val=-1;
-        }        
-        break;        
-    }while(val=1);
-    
+        break;
+    } while (val = 1);
+
     return 0;
 }
 
