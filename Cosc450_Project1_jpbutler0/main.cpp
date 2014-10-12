22cd190f0dcf8772d410b4fe4a41d0f593d67041
alfred <jonpbiv@gmail.com>
2014-10-12 02:49:13 -0400

    updated

--- Cosc450_Project1_jpbutler0/main.cpp
+++ Cosc450_Project1_jpbutler0/main.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <cstdlib>
+#include<stdio.h>
+#include<string.h>
 
 using namespace std;
 
@@ -14,6 +16,30 @@ using namespace std;
  */
 int main(int argc, char** argv) {
 
+    int val=1;
+    FILE *file=NULL;
+    
+    do{
+        
+        char location[256];
+        printf("Please input the location of the data.txt file: ");
+        fgets(location, 256, stdin);
+        strcpy(location,"/home/alfred/Desktop/data.txt");        
+        printf("You have entered: %s \n",location);        
+        file=fopen(location,"r");
+               
+        if(file==NULL){
+            printf("ERROR: failed to find data.txt file, please try again");
+        }
+        else{
+            printf("PASS: data.txt file has been found.");
+            
+            
+            val=-1;
+        }        
+        break;        
+    }while(val=1);
+    
     return 0;
 }
 
